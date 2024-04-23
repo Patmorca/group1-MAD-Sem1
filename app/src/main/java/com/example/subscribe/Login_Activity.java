e2f196cd54de5fbd194787557dea3e56dce23659
huydeptrai <trinhvanhuy11a9@gmail.com>
2024-04-23 18:13:31 +1000

    Login and Register Function completed

--- app/src/main/java/com/example/subscribe/Login_Activity.java
+++ app/src/main/java/com/example/subscribe/Login_Activity.java
@@ -1,6 +1,11 @@
 package com.example.subscribe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,57 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login_Activity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class Login_Activity extends AppCompatActivity {
+    Button Login , Register;
+    EditText setEmail , setPass;
+
+    // Firebase Auth
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser firebaseUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Login = findViewById(R.id.AL_Loginbt);
+        Register = findViewById(R.id.AL_Registerbt);
+        setEmail = findViewById(R.id.Al_EmaiL);
+        setPass = findViewById(R.id.AL_Password);
+
+        Register.setOnClickListener(v -> {
+            Intent i = new Intent(Login_Activity.this, SignUp_Activity.class);
+            startActivity(i);
         });
+
+        // Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        Login.setOnClickListener(v -> {
+            login(
+                    setEmail.getText().toString().trim(),
+                    setPass.getText().toString().trim()
+            );
+        });
+    }
+
+    private void login(String email ,  String pass) {
+        if(!TextUtils.isEmpty(email) && ! TextUtils.isEmpty(email)){
+            firebaseAuth.signInWithEmailAndPassword(email , pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    Intent i = new Intent(Login_Activity.this , MainActivity.class);
+                    startActivity(i);
+                }
+            });
+        }
     }
 }
---
+++ app/src/main/java/com/example/subscribe/SignUp_Activity.java
@@ -0,0 +1,103 @@
+package com.example.subscribe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class SignUp_Activity extends AppCompatActivity {
+    EditText setName, setEmail , setPass;
+    Button Registerbt, Backbt;
+
+    // Firebase Auth
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    private FirebaseUser currentUser;
+    // Firebase connection
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("User");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up);
+        setName = findViewById(R.id.SUA_Name);
+        setEmail = findViewById(R.id.SUA_Email);
+        setPass = findViewById(R.id.SUA_PassW);
+        Registerbt = findViewById(R.id.SUA_Registerbt);
+        Backbt = findViewById(R.id.SUA_Backbt);
+
+
+        Backbt.setOnClickListener(v -> {
+            Intent i = new Intent(SignUp_Activity.this, Login_Activity.class);
+            startActivity(i);
+        });
+        // firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+
+                if(currentUser != null){
+                    // already log in
+                } else{
+                    // sign out
+                }
+            }
+        };
+
+        Registerbt.setOnClickListener(v -> {
+            if(!TextUtils.isEmpty(setName.getText().toString()) && !TextUtils.isEmpty(setEmail.getText().toString()) && !TextUtils.isEmpty(setPass.getText().toString())){
+                String email = setEmail.getText().toString().trim();
+                String name = setName.getText().toString().trim();
+                String pass = setPass.getText().toString().trim();
+                CreateUserEmailAcc(name,  email , pass);
+            }else{
+                Toast.makeText(SignUp_Activity.this,
+                        "No Missing Field",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private void CreateUserEmailAcc(String name, String email, String pass) {
+        if(!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass) ){
+            firebaseAuth.createUserWithEmailAndPassword(
+                    email, pass
+            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        // Account Successfull
+
+                        Toast.makeText(SignUp_Activity.this , "Successfully" , Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+    }
+}
