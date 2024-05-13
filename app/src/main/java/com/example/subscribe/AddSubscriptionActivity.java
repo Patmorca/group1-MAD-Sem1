9a3c99bb31a99f542a78eca3ea19d4c6cfeb5b18
Patmorca <patmika1@gmail.com>
2024-05-13 23:32:42 +1000

    User Settings

--- app/src/main/java/com/example/subscribe/AddSubscriptionActivity.java
+++ app/src/main/java/com/example/subscribe/AddSubscriptionActivity.java
@@ -10,7 +10,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
--- app/src/main/java/com/example/subscribe/UserSettingsActivity.java
+++ app/src/main/java/com/example/subscribe/UserSettingsActivity.java
@@ -22,7 +22,6 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.auth.User;
 
 public class UserSettingsActivity extends AppCompatActivity {
 
