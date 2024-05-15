a22cbb267c24e30166e7490a212f411a3da39050
huydeptrai <trinhvanhuy11a9@gmail.com>
2024-05-15 23:01:43 +1000

    progress bar in login

--- app/src/main/java/com/example/subscribe/Login_Activity.java
+++ app/src/main/java/com/example/subscribe/Login_Activity.java
@@ -3,16 +3,15 @@ package com.example.subscribe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -22,6 +21,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class Login_Activity extends AppCompatActivity {
     Button Login , Register, Crash;
     EditText setEmail , setPass;
+    ProgressBar progressBar;
 
     // Firebase Auth
 
@@ -36,6 +36,8 @@ public class Login_Activity extends AppCompatActivity {
         Register = findViewById(R.id.AL_Registerbt);
         setEmail = findViewById(R.id.Al_EmaiL);
         setPass = findViewById(R.id.AL_Password);
+        progressBar = findViewById(R.id.AL_progressBar);
+
 
         Register.setOnClickListener(v -> {
             Intent i = new Intent(Login_Activity.this, SignUp_Activity.class);
@@ -65,15 +67,18 @@ public class Login_Activity extends AppCompatActivity {
 
     private void login(String email ,  String pass) {
         if(!TextUtils.isEmpty(email) && ! TextUtils.isEmpty(email)){
+            progressBar.setVisibility(View.VISIBLE); // Show the progress bar
             firebaseAuth.signInWithEmailAndPassword(email , pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                 @Override
                 public void onSuccess(AuthResult authResult) {
+                    progressBar.setVisibility(View.GONE);
                     Intent i = new Intent(Login_Activity.this , MainActivity.class);
                     startActivity(i);
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
+                    progressBar.setVisibility(View.GONE);
                     Toast.makeText(Login_Activity.this, "Wrong email or password" , Toast.LENGTH_SHORT).show();
                 }
             });
